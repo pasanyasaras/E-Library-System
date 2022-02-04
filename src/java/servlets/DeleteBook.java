ef9daed1bd9bbbcca535c5bd109ddc11fded3ac0
pasanyasaras <pasansmart@gmail.com>
2022-02-04 13:11:55 +0530

    creating controlling servlets

---
+++ src/java/servlets/DeleteBook.java
@@ -0,0 +1,24 @@
+
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import dao.*;
+import javax.servlet.annotation.WebServlet;
+/**
+ *
+ * @author Pasan Yasara
+ */
+
+@WebServlet("/servlets.DeleteBook")
+public class DeleteBook extends HttpServlet {
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		BookDao.delete(request.getParameter("callno"));
+		response.sendRedirect("servlets.ViewBook");
+	}
+   
+}
